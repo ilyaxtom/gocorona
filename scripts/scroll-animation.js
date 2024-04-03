991a52a44b314a9f0f8150c91ef5dff65e87c377
ilyaxtom <xtom38@gmail.com>
2024-04-03 23:18:33 +0300

    scroll animation

---
+++ scripts/scroll-animation.js
@@ -0,0 +1,29 @@
+const animatedElements = document.querySelectorAll('.animateElement');
+
+function animate() {
+    let windowHeight = window.innerHeight;
+    let scrollPosition = window.scrollY;
+
+    animatedElements.forEach(function (element) {
+        let elementPosition = element.offsetTop;
+
+        if (scrollPosition > elementPosition - windowHeight + 100) {
+            element.classList.add('animated');
+        }
+    });
+}
+
+animate();
+
+window.addEventListener('scroll', function () {
+    let windowHeight = window.innerHeight;
+    let scrollPosition = window.scrollY;
+
+    animatedElements.forEach(function (element) {
+        let elementPosition = element.offsetTop;
+
+        if (scrollPosition > elementPosition - windowHeight + 100) {
+            element.classList.add('animated');
+        }
+    });
+});
